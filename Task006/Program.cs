b33aa6196acc78ab6c31b0f17a9a4c7e0d8a171f
Eduard Timkin <vxnaotsugu@gmail.com>
2023-03-16 18:17:24 +0700

    and Task006

--- Task006/Program.cs
+++ Task006/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+//float result = number / 2;
+if (number % 2 == 0)
+{
+    Console.WriteLine("Да"); 
+}
+else 
+{
+    Console.WriteLine("Нет"); 
+}
