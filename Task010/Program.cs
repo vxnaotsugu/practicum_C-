fb2ca121d26188b014605eacc3b895b5824feb22
Eduard Timkin <vxnaotsugu@gmail.com>
2023-03-16 23:16:49 +0700

    and Task0010

--- Task010/Program.cs
+++ Task010/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Введите трехзначное число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 / 1000 > 1)
+{
+    if (number1 / 10000 > 1)
+    {
+        
+    }
+    number1 = number1 % 100;
+    number1 = number1 / 10;
+    Console.WriteLine(number1);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
