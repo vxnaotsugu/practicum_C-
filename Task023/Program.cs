e7c22d77d86d6e89949d9b1ea21a3944a7fdf210
Eduard Timkin <vxnaotsugu@gmail.com>
2023-03-22 22:59:58 +0700

    and Task021

--- Task023/Program.cs
+++ Task023/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.WriteLine("Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N");
+Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+int b = 0;
+while (a <= n) 
+{
+    b = a*a*a;
+    Console.Write($"{b}, ");
+    a += 1;
+}
